dc5a6dd436e803361117a346211fb4db2aef1167
Marcelo Veras <marceloantonino.verass@gmail.com>
2022-11-14 20:26:36 -0300

    activities

---
+++ aula49/index.js
@@ -0,0 +1,17 @@
+function falaOi() {
+    console.log('salve cria');
+}
+
+falaOi();
+
+const funcaoQualquer = function () {
+    console.log('salve cria2');
+}
+
+funcaoQualquer();
+
+const outraFuncao = () => {
+    console.log('salve cria3');
+}
+
+outraFuncao();
